67811016502f3c9e5f330f5b17432340cf59b5b2
Will <wilmerpena2908@gmail.com>
2024-09-13 20:03:10 -0500

    Cambios al perfil de la web

--- initialize.php
+++ initialize.php
@@ -1,6 +1,6 @@
 <?php
 $dev_data = array('id' => '1', 'firstname' => 'Jhoel', 'lastname' => 'Perez', 'username' => 'tareacompleto', 'password' => '21232f297a57a5a743894a0e4a801fc3', 'last_login' => '', 'date_updated' => '', 'date_added' => '');
-if (!defined('base_url')) define('base_url', 'http://localhost/sisinventarios/');
+if (!defined('base_url')) define('base_url', 'http://localhost:8081/sisinventarios/');
 if (!defined('base_app')) define('base_app', str_replace('\\', '/', __DIR__) . '/');
 if (!defined('dev_data')) define('dev_data', $dev_data);
 if (!defined('DB_SERVER')) define('DB_SERVER', "localhost");
